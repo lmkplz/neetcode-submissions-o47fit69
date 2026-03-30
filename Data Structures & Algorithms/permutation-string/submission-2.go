bab8484abbb6748e7b5b5ea31c039eaf6240880d
lmkplz <jm.kryjak007@outlook.com>
2026-03-30 07:54:28 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.go
@@ -0,0 +1,34 @@
+func checkInclusion(s1 string, s2 string) bool {
+	l := 0
+	defaultHash := make(map[byte]int)
+
+	for l = 0; l < len(s1); l++ {
+		defaultHash[s1[l]]++
+	}
+
+	for l = 0; l < len(s2) - len(s1) + 1; l++ {
+		tmp := s2[l:l+len(s1)]
+		hash := make(map[byte]int)
+
+		for i := 0; i < len(tmp); i++ {
+			hash[tmp[i]]++
+		}
+
+		if len(hash) != len(defaultHash) {
+			continue
+		}
+
+		numOfChars := 0
+
+		for key, val := range hash {
+			if val == defaultHash[key] {
+				numOfChars++
+			}
+		}
+		if numOfChars == len(hash) {
+			return true
+		}
+	}
+
+	return false
+}
