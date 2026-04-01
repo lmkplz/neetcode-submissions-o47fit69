3ed0a1d3f205165a2c0812896afb815deffe2f5f
lmkplz <jm.kryjak007@outlook.com>
2026-04-01 04:47:28 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.go
@@ -0,0 +1,19 @@
+func isValid(s string) bool {
+    stack := []byte{}
+	closeToOpen := map[byte]byte{')': '(', ']': '[', '}': '{'}
+
+	for i:=0; i<len(s); i++ {
+		if _, exists := closeToOpen[s[i]]; !exists {
+			stack = append(stack, s[i])
+			continue
+		}
+		if len(stack) > 0 && stack[len(stack)-1] == closeToOpen[s[i]] {
+			stack = stack[0:len(stack)-1]
+			fmt.Println(stack)
+		} else {
+			return false
+		}
+	}
+
+	return len(stack) == 0
+}
