2895bcedba856c9f9b32e641cb8142b3d08de13e
lmkplz <jm.kryjak007@outlook.com>
2026-04-02 05:06:07 -0400

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.go
@@ -0,0 +1,37 @@
+func evalRPN(tokens []string) int {
+	stack := []int{}
+	for i:=0; i<len(tokens); i++ {
+		num, err := strconv.Atoi(tokens[i])
+		
+		// if number, push to stack
+		if err == nil {
+			stack = append(stack, num)
+			continue
+		}
+
+		// if operator
+		val1 := stack[len(stack)-2]
+		val2 := stack[len(stack)-1]
+		res := 0
+
+		if tokens[i] == "+" {
+			res = val1 + val2
+		}
+		if tokens[i] == "-" {
+			res = val1 - val2
+		}
+		if tokens[i] == "*" {
+			res = val1 * val2
+		}
+		if tokens[i] == "/" {
+			if val2 == 0 {
+				return math.MaxInt
+			}
+			res = val1 / val2
+		}
+		stack = stack[:len(stack)-2]
+		stack = append(stack, res)
+	}
+
+	return stack[0]
+}
