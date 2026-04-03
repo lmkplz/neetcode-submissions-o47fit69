055bc2752f044fee2a30283298ab4bb0e85e89c4
lmkplz <jm.kryjak007@outlook.com>
2026-04-03 08:26:50 -0400

    Add: largest-rectangle-in-histogram - submission-1

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-1.go
@@ -0,0 +1,29 @@
+func largestRectangleArea(heights []int) int {
+	n := len(heights)
+	stack := make([][2]int, n)
+	max := 0
+
+	for i := 0; i < n; i++ {
+		cur := [2]int{i, heights[i]}
+		for len(stack) > 0 && stack[len(stack) - 1][1] > cur[1] {
+			size := stack[len(stack)-1][1] * (i - stack[len(stack)-1][0])
+			if max < size {
+				max = size
+			}
+
+			cur[0] = stack[len(stack)-1][0]
+			stack = stack[:len(stack)-1]
+		}
+
+		stack = append(stack, cur)
+	}
+
+	for _, pair := range stack {
+		area := pair[1] * (n - pair[0])
+		if max < area {
+			max = area
+		}
+	}
+
+	return max
+}
