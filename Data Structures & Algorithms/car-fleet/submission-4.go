1bce099acce73d8225413a47f13c85404632fa9d
lmkplz <jm.kryjak007@outlook.com>
2026-04-03 06:07:01 -0400

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.go
@@ -0,0 +1,31 @@
+func carFleet(target int, position []int, speed []int) int {
+	sortedTime := make([]float32, len(position))
+	stack := []float32{}
+
+	for i:=0; i<len(position); i++ {
+		for j:=i+1; j<len(position); j++ {
+			if position[i] < position[j] {
+				tmp := position[j]
+				position[j] = position[i]
+				position[i] = tmp
+
+				tmp = speed[j]
+				speed[j] = speed[i]
+				speed[i] = tmp
+			}
+		}
+		sortedTime[i] = (float32(target) - float32(position[i])) / float32(speed[i])
+	}
+
+	for k, t := range sortedTime {
+		if k == 0 {
+			stack = append(stack, t)
+			continue
+		}
+		if len(stack) > 0 && stack[len(stack)-1] < t {
+			stack = append(stack, t)
+		}
+	}
+
+	return len(stack)
+}
